4bb06f09701df1121fdce6b0dfcd8f59b021886d
HIHJH <2170095@ewhain.net>
2024-09-14 02:31:08 +0900

    init: 페이지 세팅

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Landing = () => {
+  return <Container>아아아안녕하세요</Container>;
+};
+
+export default Landing;
+
+const Container = styled.div`
+  width: 100vw;
+  overflow: hidden;
+`;
