66592fe7af25b6ad67bcf52234977e97b67e2592
HIHJH <2170095@ewhain.net>
2024-09-14 02:30:51 +0900

    init: 디자인 세팅

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    font-family: 'Pretendard';
+    font-weight: 400;
+  }
+`;
+
+export default GlobalStyles;
---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  bg: "#F5F5F5",
+  Sub_1: "#FFAF52",
+  Sub_2: "#FFCEAA",
+  Sub_3: "#FFE0C8",
+  Sub_4: "#FEF1E8",
+  primary: "#FF6B00",
+  white: "#FFFFFF",
+  gray800: "#666666",
+  gray900: "#272727",
+} as const;
