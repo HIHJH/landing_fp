cec42048d42947e2f0b0a429d66ab075b9445451
HIHJH <2170095@ewhain.net>
2024-09-17 07:00:16 +0900

    feat: 모달추가

---
+++ src/styles/ModalStyle.tsx
@@ -0,0 +1,38 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import { media } from "./media";
+
+// 모달 창 뒷배경
+export const SearchModalBox = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 999;
+`;
+
+// 모달 스타일
+export const SearchModalContent = styled(motion.div)`
+  font-family: Pretendard;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 3.19rem 6.25rem;
+  width: 20rem;
+  height: 10rem;
+  border-radius: 1.25rem;
+  background: #fff;
+  z-index: 5;
+
+  ${media.phone`
+  width: 70vw;
+    height: auto;
+    padding: 2rem;
+  `}
+`;
