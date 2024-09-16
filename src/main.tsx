4f44884ef936b195eac03cbeaef39b3869e78845
HIHJH <2170095@ewhain.net>
2024-09-16 23:09:15 +0900

    feat: analytics 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 import "@fonts/fonts.css";
 import GlobalStyles from "./styles/GlobalStyles.ts";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GlobalStyles />
     <App />
+    <Analytics />
   </StrictMode>
 );
